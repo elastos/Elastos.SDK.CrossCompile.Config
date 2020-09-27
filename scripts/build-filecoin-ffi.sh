e5b4326631de826ba1dfadfdd7350ed4b2d47d9d
mengxk <xiaokun.meng@qcast.cn>
2020-09-27 20:25:00 +0800

    scripts: add build-filecoin-ffi.sh.

---
+++ scripts/build-filecoin-ffi.sh
@@ -0,0 +1,113 @@
+#!/bin/bash
+
+set -o errexit
+set -o nounset
+
+KERNEL_NAME=$(uname -s);
+SED_CMD='sed -i';
+if [ "$KERNEL_NAME" == "Darwin" ]; then
+    SED_CMD+=" .sed.bak";
+fi
+
+check_cargo()
+{
+    if [ -z "$(which cargo)" ]; then
+        echo "You have to install cargo first"
+        exit 1
+    fi
+    CARGO_PATH=$(which cargo);
+
+    loginfo "Found cargo at [$CARGO_PATH]";
+
+    CARGO_CFG_PATH="$(dirname "$CARGO_PATH")/../config";
+    echo "[target.${ANDROID_TOOLCHAIN}]"  >  "$CARGO_CFG_PATH";
+    echo "ar='$CC'"                         >> "$CARGO_CFG_PATH";
+    echo "linker='$CC'"                     >> "$CARGO_CFG_PATH";
+}
+
+config_tarball()
+{
+    git checkout -- .;
+
+    $SED_CMD 's|rlib|dylib|'                               rust/Cargo.toml;
+    $SED_CMD 's|^byteorder .*|#&|'                         rust/Cargo.toml;
+    $SED_CMD 's|^drop_struct_macro_derive .*|#&|'          rust/Cargo.toml;
+    $SED_CMD 's|^ff .*|#&|'                                rust/Cargo.toml;
+    $SED_CMD 's|^log .*|#&|'                               rust/Cargo.toml;
+    $SED_CMD 's|^paired .*|#&|'                            rust/Cargo.toml;
+    $SED_CMD 's|^fil_logger .*|#&|'                        rust/Cargo.toml;
+    $SED_CMD 's|^anyhow .*|#&|'                            rust/Cargo.toml;
+    $SED_CMD 's|^bellperson .*|#&|'                        rust/Cargo.toml;
+    $SED_CMD 's|^serde_json .*|#&|'                        rust/Cargo.toml;
+    $SED_CMD 's|^serde_json .*|#&|'                        rust/Cargo.toml;
+    $SED_CMD 's|^neptune .*|#&|'                           rust/Cargo.toml;
+    $SED_CMD 's|^neptune-triton .*|#&|'                    rust/Cargo.toml;
+    $SED_CMD 's|^\[dependencies.filecoin-proofs-api\]|#&|' rust/Cargo.toml;
+    $SED_CMD 's|^package = "filecoin-proofs-api"|#&|'      rust/Cargo.toml;
+    $SED_CMD 's|^version = "5.1.1"|#&|'                    rust/Cargo.toml;
+    $SED_CMD 's|^\[dev-dependencies\]|#&|'                 rust/Cargo.toml;
+    $SED_CMD 's|^tempfile = ".*"|#&|'                      rust/Cargo.toml;
+
+    $SED_CMD 's|^pub mod proofs;||'                        rust/src/lib.rs;
+    $SED_CMD 's|^pub mod util;||'                          rust/src/lib.rs;
+
+    $SED_CMD "s|^use crate::proofs::types::fil_32ByteArray;|#[repr(C)] pub struct fil_32ByteArray { pub inner: [u8; 32], }|g" \
+                                                           rust/src/bls/api.rs;
+}
+
+build_tarball()
+{
+	mkdir -p "$BUILD_DIR" && cd "$BUILD_DIR";
+	loginfo "change directory to $BUILD_DIR";
+
+    check_cargo;
+
+	if [ ! -e "$BUILD_DIR/$FILECOIN_FFI_NAME" ]; then
+		cp -r "$BUILD_TARBALL_DIR/$FILECOIN_FFI_NAME" "$BUILD_DIR/$FILECOIN_FFI_NAME";
+	fi
+	loginfo "${FILECOIN_FFI_TARBALL//\//-} has been unpacked."
+	cd "$BUILD_DIR/$FILECOIN_FFI_NAME";
+
+	if [ ! -e ".built" ]; then
+        config_tarball;
+        pushd rust;
+        rustup target add "${ANDROID_TOOLCHAIN}";
+        cargo build --package=filcrypto --target="${ANDROID_TOOLCHAIN}" --release;
+        popd;
+        touch ".built";
+    fi
+	loginfo "$FILECOIN_FFI_TARBALL has been built."
+
+	if [ ! -e ".installed" ]; then
+        mkdir -p "$OUTPUT_DIR"/{lib,include}/;
+        cp "rust/target/${ANDROID_TOOLCHAIN}/release/libfilcrypto.a" "$OUTPUT_DIR/lib";
+        cp "$(find rust/target/${ANDROID_TOOLCHAIN}/release -name filcrypto.h)" "$OUTPUT_DIR/include";
+        touch ".installed";
+    fi
+	loginfo "$FILECOIN_FFI_TARBALL has been installed."
+}
+
+main_run()
+{
+	loginfo "parsing options";
+	getopt_parse_options $@;
+
+	source "$SCRIPT_DIR/common/setenv.sh";
+
+	source "$SCRIPT_DIR/tarball-config.sh";
+    source "$SCRIPT_DIR/download-tarball.sh";
+    local tarball_url="$FILECOIN_FFI_BASE_URL/$FILECOIN_FFI_TARBALL";
+    local tarball_version="$FILECOIN_FFI_VERSION";
+    local tarball_path="$BUILD_TARBALL_DIR/$FILECOIN_FFI_NAME";
+	clone_from_github "$tarball_url" "$tarball_path" "$tarball_version";
+
+	build_tarball $@;
+
+	loginfo "DONE !!!";
+}
+
+SCRIPT_DIR=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd);
+
+source "$SCRIPT_DIR/common/getopt.sh";
+
+main_run $@;
